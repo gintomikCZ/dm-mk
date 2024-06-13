c700e3776b94144477d1b5b826eca89ed69dc9b6
Tomas Glabazna <tomas@glabazna.cz>
2024-06-13 13:54:25 +0100

    cmt2

--- src/components/TAcc.vue
+++ src/components/TAcc.vue
@@ -1,5 +1,6 @@
 
 <template>
+  <div>
   <AccItem
     v-for="(item, index) in items"
     :header="item.header"
@@ -7,6 +8,8 @@
     :show="shows[index]"
     @clicked="onClicked(index)"
   />
+  </div>
+
 </template>
 
 
@@ -32,17 +35,9 @@ export default {
   },
   methods: {
     onClicked (index) {
-      console.log(this.shows)
-      for(let i = 0; i < this.items.length; i++) {
-        if (index === i) {
-          console.log('shoda', i)
-          this.shows[i] = !this.shows[i]
-        } else {
-          console.log('neshoda', i)
-          this.shows[i] = false
-        }
+      for(let i = 0; i < this.shows.length; i++) {
+        this.shows[i] = index === i ? !this.shows[i] : false
       }
-      console.log(this.shows)
     }
   },
   components: { AccItem }
